a92d0b425ccd9f273ceb7256bf5aaa7355e47a38
Sayan Paul <sayanp607@gmail.com>
2025-06-11 14:00:46 +0530

    Update todoService.jsx

--- src/services/todoService.jsx
+++ src/services/todoService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/todos';
+const API_URL = 'https://todo-backend-0hzo.onrender.com/api/todos';
 
 export const getTodos = () => axios.get(API_URL);
 export const createTodo = (data) => axios.post(API_URL, data);
